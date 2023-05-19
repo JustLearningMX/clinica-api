7971a4d0b7478d7b9ce28fd9b4a207d4d641cf03
Hiram Chavez <virtual.liga@gmail.com>
2023-05-19 07:54:25 -0600

    Creating project structure

---
+++ src/test/java/me/hiramchavez/clinicaapi/ClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hiramchavez.clinicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
